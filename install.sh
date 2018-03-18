96da5ee79fb5c58ee636f58fe575f82283934ad6
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-03-17 20:38:50 -0400

    Add files

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e # exit on fail
+# Symlink from ~/.dotfiles to ~/
+# Can handle nested links, e.g. symlink only .emacs.d/config
+#   but not all of .emacs.d
+
+function error_msg { echo "[ EXISTS ] $@"; }
+function success_msg { echo "[ CREATE ] $@"; }
+
+function checkedlink {
+    # Check if exists then link if not
+    fname="$destdir/$1"
+
+    # Create intermediate directories as needed
+    dname=$(dirname $fname)
+    if [[ ! -d $dname ]]
+    then
+        echo "$1: Making dirs: $dname"
+        mkdir -p $dname;
+    fi
+
+    # Attempt to create symlink, fail if a file / directory exists
+    if [ -e "$fname" ]
+    then
+        error_msg "$fname"
+        return
+    else
+        success_msg "$fname"
+        ln -s "$srcdir/$1" "$destdir/$1"
+    fi
+}
+
+srcdir=~/.dotfiles
+destdir=~
+
+#checkedlink .bashrc
+#checkedlink .bash_profile
+#checkedlink .tmux.conf
+#checkedlink .vimrc
+#checkedlink .config/nvim/init.vim
+checkedlink .zshrc
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
